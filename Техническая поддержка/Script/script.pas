e055a7e7fea5faf90e43bd0ab829ab17744d9403
salavat <i@snasyrov.ru>
2022-06-30 14:03:07 +0300

    С программой

---
+++ Техническая поддержка/Script/script.pas
@@ -0,0 +1,13 @@
+﻿var
+  sHello: string;
+
+procedure HelloWorld (s: string);
+begin
+  ShowMessage(s);
+end;
+
+begin
+  // just for example
+  sHello := 'Script said hello!';
+  HelloWorld(sHello);
+end.
